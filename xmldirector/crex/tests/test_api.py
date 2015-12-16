fbd99c0caa0614277c2235e9551a3ef7266fceb0
zopyx <yet@gmx.de>
2015-12-16 11:50:39 +0100

    fixes

--- xmldirector/crex/tests/test_api.py
+++ xmldirector/crex/tests/test_api.py
@@ -4,7 +4,6 @@ from plone.app.testing import TEST_USER_ID
 from plone.app.testing import SITE_OWNER_NAME
 from plone.app.testing import SITE_OWNER_PASSWORD
 
-from xmldirector.crex.tests.base import POLICY_INTEGRATION_TESTING
 from xmldirector.crex.tests.base import TestBase
 
 import unittest
@@ -14,11 +13,11 @@ import transaction
 
 class TestFilestreamIterator(TestBase):
 
-    layer = POLICY_INTEGRATION_TESTING
-
     def test_create(self):
         response = requests.put(
             self.portal.absolute_url() + '/xmldirector-create',
             headers={'Accept': 'application/json'},
             auth=(SITE_OWNER_NAME, SITE_OWNER_PASSWORD))
+        print response
         self.assertTrue(response.status_code == 201)
+
